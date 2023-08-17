6fef0e8839e58837d57f698dcfa57caf71446510
wolfie <rodoshiereheean@gmail.com>
2023-08-17 16:25:12 +0600

    pleb

---
+++ php/menu.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width initial-scale=1.0">
+
+
+    <title>Spice</title>
+    <meta name="title" content="Spice">
+    <meta name="description" content="Spice Website">
+    <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+    
+    
+    <section class="menus-area" style="background-image: url('img/back2.png');">
+        <div class="container">
+            <div class="section-title">
+                <h4>Menu</h4>
+    
+            </div>
+            <div class="menus">
+                <div class="menu-items">
+                    <div class="title">Appetizers</div>
+                    <div class="single-menu">
+                    
+                        <img src="image/menu-1" alt="">
+                        <span>350bdt</span>
+                        <h4>Cashewnut Salad</h4>
+                        <button>Add To Cart</button>
+                        
+                       
+
+                        
+                        
+    
+                    </div>
+                    <div class="single-menu">
+                    
+                        <img src="/image/wings.jpg" alt="">
+                        <span>165bdt</span>
+                        <h4>Buffalo Wings</h4>
+                        <button>Add To Cart</button>
+    
+                    </div>
+                    <div class="single-menu">
+                    
+                        <img src="/image/frenchfries.jpg" alt="">
+                        <span>100bdt</span>
+                        <h4>French Fries</h4>
+                        <button>Add To Cart</button>
+    
+                    </div>
+                    <div class="single-menu">
+                    
+                        <img src="/image/friend chicken.jpg" alt="">
+                        <span>110bdt</span>
+                        <h4>Fried Chicken</h4>
+                        <button>Add To Cart</button>
+    
+                    </div>
+                    <div class="single-menu">
+                    
+                        <img src="/image/onionrings.jpg" alt="">
+                        <span>150bdt</span>
+                        <h4>Onion Rings</h4>
+                        <button>Add To Cart</button>
+    
+                    </div>
+                </div>
+                <div class="menu-items">
+                    <div class="title">Main Dish</div>
+                    <div class="single-menu">
+                    
+                        <img src="image/menu-3" alt="">
+                        <span>300bdt</span>
+                        <h4>Biriyani</h4>
+                        <button>Add To Cart</button>
+    
+                    </div>
+                    <div class="single-menu">
+                    
+                        <img src="/image/menu-4.jpg" alt="">
+                        <span>650bdt</span>
+                        <h4>Classic Pizza</h4>
+                        <button>Add To Cart</button>
+    
+                    </div>
+                    <div class="single-menu">
+                    
+                        <img src="/image/menu-2.jpg" alt="">
+                        <span>670bdt</span>
+                        <h4>Lasagna</h4>
+                        <button>Add To Cart</button>
+    
+                    </div>
+                    <div class="single-menu">
+                    
+                        <img src="/image/chowmein.jpg" alt="">
+                        <span>300bdt</span>
+                        <h4>Chowmein</h4>
+                        <button>Add To Cart</button>
+    
+                    </div>
+                    <div class="single-menu">
+                    
+                        <img src="/image/pasta.jpg" alt="">
+                        <span>350bdt</span>
+                        <h4>Baked Pasta</h4>
+                        <button>Add To Cart</button>
+    
+                    </div>
+                </div>
+                <div class="menu-items">
+                    <div class="title">Desserts</div>
+                    <div class="single-menu">
+                    
+                        <img src="/image/cheesecake.jpg" alt="">
+                        <span>400bdt</span>
+                        <h4>Cheese Cake</h4>
+                        <button>Add To Cart</button>
+    
+                    </div>
+                    <div class="single-menu">
+                    
+                        <img src="/image/brownie.jpg" alt="">
+                        <span>150bdt</span>
+                        <h4>Brownie</h4>
+                        <button>Add To Cart</button>
+    
+                    </div>
+                    <div class="single-menu">
+                    
+                        <img src="/image/pudding.jpg" alt="">
+                        <span>200bdt</span>
+                        <h4>Pudding</h4>
+                        <button>Add To Cart</button>
+    
+                    </div>
+                    
+                    
+                </div>
+                <div class="menu-items">
+                    <div class="title">Drinks</div>
+                    <div class="single-menu">
+                    
+                        <img src="/image/coldcoffe.jpg" alt="">
+                        <span>150bdt</span>
+                        <h4>Cold Coffee</h4>
+                        <button>Add To Cart</button>
+    
+                    </div>
+                    <div class="single-menu">
+                    
+                        <img src="/image/milkshake.jpg" alt="">
+                        <span>200bdt</span>
+                        <h4>Milk Shake</h4>
+                        <button>Add To Cart</button>
+    
+                    </div>
+                    <div class="single-menu">
+                    
+                        <img src="/image/juice.jpg" alt="">
+                        <span>150bdt</span>
+                        <h4>Fresh Juice</h4>
+                        <button>Add To Cart</button>
+    
+                    </div>
+                    
+                    
+                </div>
+            </div>
+        </div>
+            
+             
+   
+        
+    </section>
+    
+</body>
