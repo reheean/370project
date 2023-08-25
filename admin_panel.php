481e5cdb90843b4b6426d8a0f216bfd255e34b1a
wolfie <rodoshiereheean@gmail.com>
2023-08-25 23:47:05 +0600

    cancer

--- admin_panel.php
+++ admin_panel.php
@@ -30,7 +30,7 @@ if(mysqli_query($conn, $insertQuery)) {
    <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Project Spice Kitchen Admin Panel</title>
+      <title>Spice Admin Panel</title>
       <link rel="stylesheet" href="/370project/css/admin.css">
    </head>
    <body>
