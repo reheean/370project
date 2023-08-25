92dcf073465dfdaa5244e471b8531e9d6992dab4
wolfie <rodoshiereheean@gmail.com>
2023-08-26 01:03:00 +0600

    cancer

--- menu.php
+++ menu.php
@@ -31,6 +31,7 @@
     - custom css link
   -->
     <link rel="stylesheet" href="/370project/css/style1.css">
+    <link rel="stylesheet" href="/370project/css/header.css">
 
   <!-- 
     - preload images
@@ -38,94 +39,11 @@
 </head>
 
 <body>
-
+<?php include_once 'header.php'; ?>
 <div class="preload" data-preaload>
         <div class="circle"></div>
         <p class="text">Spice</p>
-</div>
-
-<header class="header" data-header>
-      <div class="container">
-        
-      <a href="#" class="logo">
-          <img src="/370project/image/logo.svg" width="160" height="50" alt="Spice - Home">
-      </a>
-
-      <ul class="navbar-list">
-
-          <li class="navbar-item">
-              <a href="/370project/user.php" class="navbar-link hover-underline active">
-                  <div class="separator"></div>
-
-                  <span class="span">Home</span>
-              </a>
-          </li>
-
-          <li class="navbar-item">
-              <a href="#" class="navbar-link hover-underline">
-                  <div class="separator"></div>
-
-                  <span class="span">Menus</span>
-              </a>
-          </li>
-
-          <li class="navbar-item">
-              <a href="/370project/user.php" class="navbar-link hover-underline">
-                  <div class="separator"></div>
-
-                  <span class="span">About Us</span>
-              </a>
-          </li>
-
-          <li class="navbar-item">
-              <a href="/370project/user.php" class="navbar-link hover-underline">
-                  <div class="separator"></div>
-
-                  <span class="span">Contact</span>
-              </a>
-          </li>
-
-      </ul>
-
-      <div class="text-center">
-          <p class="headline-1 navbar-title">Visit Us</p>
-
-              <address class="body-4">
-                  123 st, Dhaka, Bangladesh
-              </address>
-
-          <p class="body-4 navbar-text">Open: 8.00 am to 10.00 pm</p>
-
-          <a href="mailto:booking@grilli.com" class="body-4 sidebar-link">booking@spice.com</a>
-
-          <div class="separator"></div>
-
-          <p class="contact-label">Booking Request</p>
-
-          <a href="tel:+8801234567890" class="body-1 contact-number hover-underline">
-              +8801234567890
-          </a>
-      </div>
-
-      </nav>
-
-      <a href="/370project/checkout.php" class="btn btn-secondary">
-          <span class="text text-1">Checkout</span>
-
-          <span class="text text-2" aria-hidden="true">Checkout</span>
-      </a>
-
-      <button class="nav-open-btn" aria-label="open menu" data-nav-toggler>
-          <span class="line line-1"></span>
-          <span class="line line-2"></span>
-          <span class="line line-3"></span>
-      </button>
-
-      <div class="overlay" data-nav-toggler data-overlay></div>
-
-      </div>
-  </header>
-    
+</div> 
     <section class="menus-area" style="background-image: url('/370project/image/back2.png');">
         <div class="container">
             <div class="section-title">
